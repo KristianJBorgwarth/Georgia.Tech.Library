eb7f7282a7f76448d89dd284a5d9ac6f77627862
Kristian <fich2402@gmail.com>
2024-11-29 16:45:38 +0100

    setup common projects for clean arch

---
+++ GTL.Application/Abstractions/IDomainEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using GTL.Domain.Events;
+using MediatR;
+
+namespace GTL.Application.Abstractions;
+
+public interface IDomainEventHandler<in TDomainEvent> : INotificationHandler<TDomainEvent> where TDomainEvent : IDomainEvent
+{
+
+}
