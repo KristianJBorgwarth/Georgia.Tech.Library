54f8d88f086fa4c5a6a5ad5ca990f9be5ae19fdb
Kristian <fich2402@gmail.com>
2024-12-04 14:57:59 +0100

    added some messages

---
+++ GTL.Messaging.RabbitMq/Messages/CustomerMessages/CustomerDeletedMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace GTL.Messaging.RabbitMq.Messages.CustomerMessages;
+
+public class CustomerDeletedMessage : BaseMessage
+{
+    public Guid CustomerId { get; init; }
+
+    public CustomerDeletedMessage(Guid customerId, Guid correlationId,
+        Guid? causationId = null) : base(correlationId, causationId)
+    {
+        CustomerId = customerId;
+    }
+}
