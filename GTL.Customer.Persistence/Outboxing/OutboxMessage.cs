f02626d0b24111e340a77ffba192406b17ea4d3e
Kristian <fich2402@gmail.com>
2024-12-05 16:27:26 +0100

    added migration for outbox message

---
+++ GTL.Customer.Persistence/Outboxing/OutboxMessage.cs
@@ -0,0 +1,31 @@
+﻿// ReSharper disable EntityFramework.ModelValidation.UnlimitedStringLength
+namespace GTL.Customer.Persistence.Outboxing;
+
+public class OutboxMessage
+{
+    public Guid Id { get; init; }
+    public string Type { get; init; }
+    public string Content { get; init; }
+    public DateTime OccurredOn { get; init; }
+    public DateTime? ProcessedOn { get; private set; }
+    public string? Error { get; private set; } = string.Empty;
+
+    public OutboxMessage(string type, string content)
+    {
+        Id = Guid.NewGuid();
+        Type = type;
+        Content = content;
+        OccurredOn = DateTime.UtcNow;
+    }
+
+    public void MarkAsProcessed()
+    {
+        ProcessedOn = DateTime.UtcNow;
+    }
+
+    public void MarkAsFailed(string error)
+    {
+        ProcessedOn = DateTime.UtcNow;
+        Error = error;
+    }
+}
