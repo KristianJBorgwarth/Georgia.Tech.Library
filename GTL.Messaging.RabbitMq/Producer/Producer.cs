56318055aa3540890536de7d0c6285d9e4913476
Kristian <fich2402@gmail.com>
2024-12-05 15:50:33 +0100

    extended producer to allow for ptp with send

--- GTL.Messaging.RabbitMq/Producer/Producer.cs
+++ GTL.Messaging.RabbitMq/Producer/Producer.cs
@@ -8,11 +8,13 @@ public class Producer<TMessage> : IProducer<TMessage> where TMessage : BaseMessa
 {
     private readonly IPublishEndpoint _publishEndpoint;
     private readonly ILogger<Producer<TMessage>> _logger;
+    private readonly ISendEndpointProvider _sendEndpointProvider;
 
-    public Producer(IPublishEndpoint publishEndpoint, ILogger<Producer<TMessage>> logger)
+    public Producer(IPublishEndpoint publishEndpoint, ILogger<Producer<TMessage>> logger, ISendEndpoint sendEndpoint, ISendEndpointProvider sendEndpointProvider)
     {
         _publishEndpoint = publishEndpoint;
         _logger = logger;
+        _sendEndpointProvider = sendEndpointProvider;
     }
 
     public async Task PublishMessageAsync(TMessage message, CancellationToken cancellationToken = default)
@@ -26,6 +28,22 @@ public class Producer<TMessage> : IProducer<TMessage> where TMessage : BaseMessa
             _logger.LogCritical(ex, "Exception occured in {Producer} for message with id: {Id}",
                 nameof(Producer<TMessage>),
                 message.Id);
+        }
+    }
+
+    public async Task SendMessageASync(TMessage message, string queueName, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var endpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{queueName}"));
+            await endpoint.Send(message, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogCritical(ex, "Exception occurred in {Producer} while sending message with id: {Id} to queue: {Queue}",
+                nameof(Producer<TMessage>),
+                message.Id,
+                queueName);
         }
     }
 }
