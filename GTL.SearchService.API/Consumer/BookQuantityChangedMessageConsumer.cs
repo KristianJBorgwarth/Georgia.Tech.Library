2b821762d00bc35d1b5b9777c17f2133f5603cfa
AnnetteSOlesen <162964915+AnnetteSOlesen@users.noreply.github.com>
2024-12-10 11:09:02 +0100

    Consumer added BookQuantityChanged

---
+++ GTL.SearchService.API/Consumer/BookQuantityChangedMessageConsumer.cs
@@ -0,0 +1,16 @@
+﻿using GTL.Messaging.RabbitMq.Messages.BookMessages;
+using GTL.Messaging.RabbitMq.Messages.OrderMessages;
+using MassTransit;
+
+namespace GTL.SearchService.API.Consumer
+{
+    public class BookQuantityChangedMessageConsumer : IConsumer<BookQuantityChangedMessage>
+    {
+        public async Task Consume(ConsumeContext<BookQuantityChangedMessage> context)
+        {
+            //Implement logic to adjust cache
+            Console.WriteLine("BookId: {0}", context.Message.BookId);
+            Console.WriteLine("Quantity: {0}", context.Message.Quantity);
+        }
+    }
+}
