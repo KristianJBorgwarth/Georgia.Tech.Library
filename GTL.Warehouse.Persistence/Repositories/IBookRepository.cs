4402f13bbbf5c78a4cfe53788bb124a073916e36
Oliverden3 <90378240+Oliverden3@users.noreply.github.com>
2024-12-12 10:53:26 +0100

    Ready for shipping (#9)
    * Added persistence
    
    * Fixed branch
    
    * TODO list and restored controller
    
    * Updated controller
    
    * Update book entity to now contain UserId.
    Updated location of persistence project in folder.
    
    * Updated Book to contain a UserId. Created new Endpoints
    
    * Warehouse update
    
    * Fixed entitites

---
+++ GTL.Warehouse.Persistence/Repositories/IBookRepository.cs
@@ -0,0 +1,19 @@
+﻿using GTL.Warehouse.Persistence.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTL.Warehouse.Persistence.Repositories
+{
+    public interface IBookRepository
+    {
+        Task AddAsync(Book? book);
+        Task<Book?> GetBookByBookIdAsync(Guid bookId);
+        Task<List<Book?>> GetBookByTitleAsync(string title);
+        Task<List<Book?>> GetAllBooks();
+        Task DeleteBookWithUserIdAsync(Guid userID);
+        Task<List<Book?>> GetBooksByUserIdAsync(Guid id);
+    }
+}
