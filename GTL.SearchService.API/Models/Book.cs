b22e21d02f621696f2a318d6dde295a8b5408814
AnnetteSOlesen <162964915+AnnetteSOlesen@users.noreply.github.com>
2024-12-12 11:20:14 +0100

    search in main (#10)
    * Changed Application name in appsetting
    
    * Redis cache implemented
    
    * Consumer added BookQuantityChanged

---
+++ GTL.SearchService.API/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace GTL.SearchService.API.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string ISBN { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int AmountInStock { get; set; }
+    }
+}
